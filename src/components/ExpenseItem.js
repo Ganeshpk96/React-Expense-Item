13dfc7e9aeeae2e2446f711a93843799781a991c
Ganeshpk96 <121109471+Ganeshpk96@users.noreply.github.com>
2023-02-08 19:35:25 +0530

    Add files via upload

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,21 @@
+function ExpenseItem() {
+	return (
+		<div>
+			<div>Date</div>
+			<div>
+				<h2>Food</h2>
+				<div>Rs10</div>
+			</div>
+			<div>
+				<h2>Petrol</h2>
+				<div>Rs100</div>
+			</div>
+			<div>
+				<h2>Movies</h2>
+				<div>Rs200</div>
+			</div>
+		</div>
+	);
+}
+
+export default ExpenseItem;
